a0e22ccd8d6d4300434e0df0c5f75f4d1a6399be
EggPudding <ksj970531@gmail.com>
2021-07-14 22:34:23 +0900

    Implement load images

---
+++ data.py
@@ -0,0 +1,31 @@
+from glob import glob
+
+from torch.utils.data import Dataset, DataLoader, Subset
+
+def integrate_segments():
+    pass
+
+class dataset(Dataset):
+    def __init__(self, img_dir, annt_dir, split='train'):
+        self.imgs = glob(f"{img_dir}/*.jpg")
+        self.annts = glob(f"{annt_dir}/**/*.png")
+
+    def __len__(self):
+        return len(self.imgs)
+
+    def __getitem__(self, idx):
+        img_path = self.imgs[idx]
+
+        pass
+
+if __name__ == '__main__':
+    print('hello, world')
+
+
+    img_dir = '/home/seongjae/Downloads/CelebAMask-HQ/CelebA-HQ-img'
+    imgs = glob(f"{img_dir}/*.jpg")
+    print(len(imgs))
+    
+    annt = '/home/seongjae/Downloads/CelebAMask-HQ/CelebAMask-HQ-mask-anno'
+    annts = glob(f"{annt}/**/*.png")
+    print(len(annts))

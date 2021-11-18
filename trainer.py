a22a9432c2d7d060013d09bfdd47b93920570362
EggPudding <ksj970531@gmail.com>
2021-11-18 21:56:05 +0900

    Implement trainer for basic train

--- trainer.py
+++ trainer.py
@@ -1,50 +1,108 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import segmentation_models_pytorch as smp
+from torch.optim.lr_scheduler import MultiStepLR
 
 import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint, ModelSummary
+from pytorch_lightning.loggers import WandbLogger
 
-from torch.utils.data import Dataset, DataLoader
-
-class LitAutoEncoder(pl.LightningModule):
-    # TODO: add lr scheduling
-    # TODO: add remote logging
-    # TODO: prepare dataset
+class SegmentModel(pl.LightningModule):
     # TODO: use epic optimizer
-    # TODO: save checkpoint
-    # TODO: printing messages along trianing
 
     def __init__(self):
         super().__init__()
-        self.encoder = nn.Sequential(
-            nn.Linear(28 * 28, 64),
-            nn.ReLU(),
-            nn.Linear(64, 3)
+        # self.save_hyperparameters()
+
+        self.model = smp.FPN(
+            encoder_name='resnet34',
+            encoder_weights='imagenet',
+            in_channels=3,
+            classes=19,
         )
-        self.decoder = nn.Sequential(
-            nn.Linear(3, 64),
-            nn.ReLU(),
-            nn.Linear(64, 28 * 28)
-        )
+        self.criterion = nn.CrossEntropyLoss()
 
     def forward(self, x):
-        # in lightning, forward defines the prediction/inference actions
-        embedding = self.encoder(x)
-        return embedding
+        x = self.model(x)
+        x = torch.argmax(x, dim=1)
+        return x
 
     def training_step(self, batch, batch_idx):
-        # TODO: implement training_step
         x, y = batch
-        x = x.view(x.size(0), -1)
-        z = self.encoder(x)
-        x_hat = self.decoder(z)
-        loss = F.mse_loss(x_hat, x)
-        return loss
+        y_hat = self.model(x)
+        loss = self.criterion(y_hat, y)
+
+        self.log("train_loss", loss, on_step=True, on_epoch=True)
+        return {"loss": loss}
 
     def validation_step(self, batch, batch_idx):
-        # TODO: implement validation_step
-        pass
+        x, y = batch
+        y_hat = self.model(x)
+        loss = self.criterion(y_hat, y)
+
+        self.log("valid_loss", loss, on_step=True, on_epoch=True)
+        return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
-        return optimizer
+        optimizer = torch.optim.Adam(self.parameters(), lr=7e-3)
+        scheduler = MultiStepLR(optimizer, milestones=[10, 60, 90], gamma=0.1)
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": scheduler,
+                "interval": "epoch",
+                "frequency": 1,
+                "monitor": "valid_loss",
+            }
+        }
+
+    # TODO: testing step
+
+
+if __name__ == '__main__':
+    # data
+    from data import MaskDataset
+    img_dir = '/home/seongjae/MyDataset/CelabA/CelebAMask-HQ/CelebA-HQ-img'    
+    annt_dir = '/home/seongjae/MyDataset/CelabA/CelebAMask-HQ/mask'
+
+    from torch.utils.data import Dataset, DataLoader, Subset
+    dataset = MaskDataset(img_dir, annt_dir)
+    # dataset = Subset(dataset, range(12))
+    data_loader = DataLoader(dataset, batch_size=4, shuffle=False, num_workers=0)
+
+    print(f"Total number of dataset: {len(dataset)}")
+
+    # training
+    pl.seed_everything(42)
+
+    lighting_model = SegmentModel()
+    callbacks = [
+        ModelCheckpoint(monitor="valid_loss"),
+        ModelSummary(),
+    ]
+    wandb_logger = WandbLogger(project="face-segment")
+    trainer = pl.Trainer(
+        max_epochs=100,
+
+        callbacks=callbacks,
+        logger=wandb_logger,
+        enable_checkpointing=True,
+
+        devices="auto",
+        accelerator="gpu",
+        strategy="dp",
+        gpus=-1,
+        auto_select_gpus=True, 
+        benchmark=True,
+        deterministic=False,
+
+        # fast_dev_run=7,
+        # overfit_batches=10,
+    )
+    trainer.fit(
+        model=lighting_model,
+        train_dataloader=data_loader,
+        val_dataloaders=data_loader,
+        # ckpt_path=".chpt", # for resuming training
+    )

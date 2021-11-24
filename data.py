7e84fdd98140c5c235597e200baa912c687b4235
EggPudding <ksj970531@gmail.com>
2021-11-24 13:43:21 +0900

    Implement data augmentation

--- data.py
+++ data.py
@@ -2,6 +2,7 @@ import torch
 from glob import glob
 from torch.utils.data import Dataset, DataLoader, Subset, random_split
 import cv2
+import random
 
 from torchvision.transforms import transforms
 
@@ -11,10 +12,25 @@ transform = transforms.Compose([
     transforms.Normalize(*normalize)
 ])
 
+# atts = [1 'skin', 2 'l_brow', 3 'r_brow', 4 'l_eye', 5 'r_eye', 6 'eye_g', 7 'l_ear', 8 'r_ear', 9 'ear_r',
+#         10 'nose', 11 'mouth', 12 'u_lip', 13 'l_lip', 14 'neck', 15 'neck_l', 16 'cloth', 17 'hair', 18 'hat']
+
 class MaskDataset(Dataset):
     def __init__(self, img_dir, annt_dir):
         self.imgs = sorted(glob(f"{img_dir}/*.jpg"))
         self.annts = sorted(glob(f"{annt_dir}/*.png"))
+
+        from data_aug import ColorJitter, HorizontalFlip, RandomScale, RandomCrop
+
+        self.train_aug = transforms.Compose([
+            ColorJitter(
+                brightness=0.5,
+                contrast=0.5,
+                saturation=0.5),
+            HorizontalFlip(),
+            RandomScale((0.75, 1.0, 1.25, 1.5, 1.75, 2.0)),
+            RandomCrop((512, 512))
+            ])
 
         print(f"Number of training images: {len(self.imgs)}")
         print(f"Number of annotation images: {len(self.annts)}")
@@ -23,8 +39,6 @@ class MaskDataset(Dataset):
         return len(self.imgs)
 
     def __getitem__(self, idx):
-        # TODO: implement data augmentation
-
         img_path = self.imgs[idx]
         img = cv2.imread(img_path)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -32,6 +46,11 @@ class MaskDataset(Dataset):
 
         label_path = self.annts[idx]
         label = cv2.imread(label_path, cv2.IMREAD_GRAYSCALE)
+        label = cv2.resize(label, dsize=(512, 512), interpolation=cv2.INTER_NEAREST)
+
+        im_lb = {'im': img, 'lb': label}
+        im_lb = self.train_aug(im_lb)
+        img, label = im_lb['im'], im_lb['lb']
 
         img = transform(img)
         label = torch.tensor(label, dtype=torch.int64)
